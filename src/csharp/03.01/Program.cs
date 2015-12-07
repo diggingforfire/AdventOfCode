f0712ac1bd903b125d1f6fab0363989a4f13d205
diggingforfire <diggingforfire@gmail.com>
2015-12-08 00:41:33 +0100

    Added 03.01

---
+++ src/csharp/03.01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("input.txt");
+
+            int x = 0;
+            int y = 0;
+
+
+            var getX = new Func<char, int>((c) => { return x += c == '<' ? -1 : (c == '>' ? 1 : 0); });
+            var getY = new Func<char, int>((c) => { return y += c == 'v' ? -1 : (c == '^' ? 1 : 0); });
+
+            var points = input.Select(c => new {X = getX(c), Y = getY(c)}).ToList();
+            int result = points.Distinct().Count();
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
