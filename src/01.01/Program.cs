889a1c3beed0bcdbdfb34587dc20c895ac593d3e
r.luijben <r.luijben@ritassist.nl>
2015-12-07 14:33:58 +0100

    Initial commit + day 01.01

---
+++ src/01.01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _01._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const char up = '(';
+            string input = File.ReadAllText("input.txt");
+            int resultFloor = input.Sum(c => c == up ? 1  : -1);
+            Console.Write(resultFloor);
+        }
+    }
+}
