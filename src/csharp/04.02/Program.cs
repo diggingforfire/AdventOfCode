1cae7cade7240c11be37117618104e7d7c28eb1e
diggingforfire <diggingforfire@gmail.com>
2015-12-10 01:55:07 +0100

    fixed 04.02 concurrency problem

--- src/csharp/04.02/Program.cs
+++ src/csharp/04.02/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace _04._02
 {
@@ -13,24 +15,41 @@ namespace _04._02
         {
             string key = File.ReadAllText("input.txt");
 
-            int i = 1;
-            int result = 0;
+            var semaphore = new Semaphore(Environment.ProcessorCount, Environment.ProcessorCount);
 
-            Parallel.For(i, Int32.MaxValue, new ParallelOptions {MaxDegreeOfParallelism = Environment.ProcessorCount},
-            (index, state) =>
+            bool found = false;
+
+            var results = new List<int>();
+
+            int i = 0;
+
+            while (!found)
             {
-                int iLocal = System.Threading.Interlocked.Increment(ref i);
-
-                var bytes = MD5.Create().ComputeHash(Encoding.Default.GetBytes(key + iLocal));
-
-                if (bytes[0] == 0 && bytes[1] == 0 && bytes[2] == 0)
+                semaphore.WaitOne();
+        
+                ThreadPool.QueueUserWorkItem((state) =>
                 {
-                    result = iLocal;
-                    state.Stop();
-                }
-            });
+                    try
+                    {
+                        int iLocal = System.Threading.Interlocked.Increment(ref i);
 
-            Console.WriteLine(result);
+                        var bytes = MD5.Create().ComputeHash(Encoding.Default.GetBytes(key + iLocal));
+
+                        if (bytes[0] == 0 && bytes[1] == 0 && bytes[2] == 0)
+                        {
+                            results.Add(iLocal);
+                            found = true;
+                        }
+                    }
+                    finally
+                    {
+                        semaphore.Release();
+                    }
+                });
+ 
+            }
+
+            Console.WriteLine(results.Min());
             Console.ReadKey();
         }
     }
