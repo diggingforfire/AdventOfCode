a26801c0acece3134b90dc623b49e86c3cb46331
diggingforfire <diggingforfire@gmail.com>
2015-12-11 23:30:23 +0100

    Added day 8

---
+++ src/csharp/08.01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _08._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            Console.WriteLine(lines.Sum(l => l.Length - Regex.Unescape(l).Length - 2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/csharp/08.02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _08._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            Console.WriteLine(lines.Sum(l => l.Replace("\\", "\\\\").Replace("\"", "\\\"").Length + 2 - l.Length));
+            Console.ReadKey();
+        }
+    }
+}
