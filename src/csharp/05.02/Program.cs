a2fe77fd3a0528b6d05776cb15f644424798b691
diggingforfire <diggingforfire@gmail.com>
2015-12-11 22:24:07 +0100

    Implemented 05.02

--- src/csharp/05.02/Program.cs
+++ src/csharp/05.02/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 
 namespace _05._02
@@ -8,32 +9,48 @@ namespace _05._02
         static void Main(string[] args)
         {
             var lines = File.ReadAllLines("input.txt");
-          
-            var res = lines.Where(line => line.Skip(1)
-                .Select((c, i) => new {l = new string(new[] {lines[0][i], c})})
-                .GroupBy(g => g.l)
-                .Any(g => g.Count() > 1)).ToList();
 
-            var ress = lines.Select(line =>
-            {
-                var grp = line.Skip(1)
-                    .Select((c, i) => new {Value = new string(new[] {line[i], c}), Index = i})
-                    .GroupBy(g => g.Value)
-                    .Where(g => g.Count() > 1)
-                    .Select(g =>
-                    new
-                    {
-                        Line = line,
-                        Pair = g
-                    });
+            // I guess we could use a regex. I wonder which is more readable right now..
 
-                return grp;
+            var matchingLines = lines
+                .Select(line =>
+                new
+                {
+                    Line = line,
+                    // select all pairs into a new object
+                    Value = line.Skip(1).Select((c, i) => new {Pair = new string(new[] {line[i], c}), Index = i})
+                })
+                .Select(n => 
+                new
+                {
+                    n.Value,
+                    n.Line,
+                    // select all pairs that appear at least twice
+                    RepeatingPairs = n.Value.GroupBy(g => g.Pair).Where(g => g.Count() >= 2)
+                })
+                .Select(n =>
+                new
+                {
+                    n.Line,
+                    n.Value,
+                    n.RepeatingPairs,
+                    IndexDifference = n.RepeatingPairs.Select(p => p.Max(g => g.Index) - p.Min(g => g.Index))
+                }
+                )
+                // at least two indices apart (so no overlapping)
+                .Where(n => n.IndexDifference.Any(i => i >= 2))
+                .Select(n => 
+                new
+                {
+                    n.Line,
+                    Value = n.Line.Skip(2).Select((c, i) => new {Tri = new string(new[] {n.Line[i], n.Line[i + 1], c}), Index = i})
+                })
+                // first and last character of triplet must match
+                .Where(n => n.Value.Any(v => v.Tri[0] == v.Tri[2]))
+                .ToList();
 
-
-            }).Where(g => g.Any());
-
-
-
+            Console.WriteLine(matchingLines.Count);
+            Console.ReadKey();
         }
     }
 }
