268338951ebd69333fd198c19d084b351961ebe9
diggingforfire <diggingforfire@gmail.com>
2015-12-15 00:28:31 +0100

    Added day 10

---
+++ src/csharp/10.02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _10._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("input.txt");
+
+            for (int p = 0, id = 0; p < 50; p++)
+                text = string.Join("", text.Select((c, i) => new { c, Id = i == text.Length - 1 ? id : c == text[++i] ? id : id++ }).GroupBy(g => g).Select(g => g.Count() + g.Key.c.ToString()));
+
+            Console.WriteLine(text.Length);
+            Console.ReadKey();
+        }
+    }
+}
